920c96dd484e51d6528fb8b1eb21b2df1d596928
aif team <AIFteam@yandex.ru>
2023-03-30 15:59:02 +0300

    fixed memory flow + proxy

--- func/getContent.js
+++ func/getContent.js
@@ -9,6 +9,7 @@ const LAUNCH_PUPPETEER_OPTS = {
 	defaultViewport: false,
 	userDataDir: './profile/',
 	args: [
+		'--proxy-server=176.124.46.105:8000',
 		'--no-sandbox',
 		'--disable-setuid-sandbox',
 		'--disable-dev-shm-usage',
@@ -18,7 +19,6 @@ const LAUNCH_PUPPETEER_OPTS = {
 	]
 }
 const PAGE_PUPPETEER_OPTS = {
-	waitUntil: 'load',
 	networkIdle2Timeout: 500,
 	timeout: 120000
 }
@@ -27,38 +27,38 @@ puppeteer.use(StealthPlugin())
 
 export async function getContent (data) {
 	const result = []
+	const browser = await puppeteer.launch(LAUNCH_PUPPETEER_OPTS)
 
 	for(const item of data) {
-
 		console.log(item['URL'])
-		const browser = await puppeteer.launch(LAUNCH_PUPPETEER_OPTS)
-
+		const page = await browser.newPage()
+		await page.authenticate({
+			username: '9UQHhE',
+			password: 'uT0VW5',
+		})
 		try {
-			const page = await browser.newPage()
-			await page.goto(item['URL'], PAGE_PUPPETEER_OPTS)
+			await page.goto(item['URL'] + '#properties', PAGE_PUPPETEER_OPTS)
+			await page.screenshot({path: 'myip.png'})
 			const content = await page.content()
 			const errorPage = await page.$('.page__404')
-			const propertyBtn = '[data-test="product-characteristics-tab"]'
-			await page.waitForSelector(propertyBtn, {timeout: 20000})
 
 			if(!errorPage) {
-				await page.click(propertyBtn)
-	
 				result.push(scraping(content, item))
 			} else {
 				console.log(`Page 404. Товар ${item['Название']} по ссылки: ${item['URL']}`)
 			}
 
 			console.log(result.length)
-			page.close()
 		}
 		catch (error) {
 			console.log('Error from gunc getContent - ' + item['URL'])
 			console.log(error);
 		}
 		finally {
-			await browser.close()
+			await page.close()
 		}
 	}
+	await browser.close()
+
 	return result
 }
